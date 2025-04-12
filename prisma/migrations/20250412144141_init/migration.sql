f2d2f92bab5f395b2c3b314716257b8cc29d6d26
Hanish0 <128824988+Hanish0@users.noreply.github.com>
2025-04-12 20:13:50 +0530

    Setup Prisma with NeonDB and initialized Resume model

---
+++ prisma/migrations/20250412144141_init/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Resume" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Resume_pkey" PRIMARY KEY ("id")
+);
